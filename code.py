1aabfc5490fcfe488ae9b98352b9162804f8ca21
Miss D <40424787+knrdsps@users.noreply.github.com>
2022-05-27 18:29:51 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,15 @@
+# Write your code here :-)
+import time
+import board
+import digitalio
+
+from digitalio import DigitalInOut, Direction, Pull
+
+led = DigitalInOut(board.A0)
+led.direction = Direction.OUTPUT
+
+led.value = True
+time.sleep(0.9)
+
+
+
