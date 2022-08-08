07fb0b4a270fdccbc1689973bbe8564c1dc7663e
Miss D <40424787+knrdsps@users.noreply.github.com>
2022-08-08 10:57:17 +0100

    Create z_neopixel_loop.py

---
+++ z_neopixel_loop.py
@@ -0,0 +1,72 @@
+import board
+import time
+import neopixel
+from digitalio import DigitalInOut, Direction, Pull
+
+pixel_pin = board.A2
+num_pixels = 8
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.05, auto_write=False)
+
+RED = (255, 0, 0)
+ORANGE = (255, 128, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
+WHITE = (255, 255, 255)
+
+switchblue = DigitalInOut(board.D1)
+switchblue.direction = Direction.INPUT
+switchblue.pull = Pull.DOWN
+
+switchgreen = DigitalInOut(board.D2)
+switchgreen.direction = Direction.INPUT
+switchgreen.pull = Pull.DOWN
+
+switchyellow = DigitalInOut(board.D3)
+switchyellow.direction = Direction.INPUT
+switchyellow.pull = Pull.DOWN
+
+switchred = DigitalInOut(board.D4)
+switchred.direction = Direction.INPUT
+switchred.pull = Pull.DOWN
+
+while True:
+    if switchred.value:
+        for i in range(5):
+            pixels[0] = (RED)
+            pixels.show()
+            time.sleep(0.5)
+            pixels[0] = (WHITE)
+            pixels.show()
+            time.sleep(0.5)
+
+    if switchyellow.value:
+        pixels.fill(YELLOW)
+        pixels.show()
+        time.sleep(0.5)
+
+    if switchgreen.value:
+        pixels.fill(GREEN)
+        pixels.show()
+        time.sleep(0.5)
+
+    if switchblue.value:
+        pixels.fill(BLUE)
+        pixels.show()
+        time.sleep(0.5)
+
+    else:
+        pixels.value = False
+        pixels.fill((0, 0, 0))
+        pixels.show()
+        time.sleep(0.01)
+
+    # for i in range(5):
+
+    # led.value = True
+    # time.sleep(0.1)
+    # led.value = False
+    # time.sleep(0.1)
